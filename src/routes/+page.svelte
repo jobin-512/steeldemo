f956b57e22d531f634383dc048bb3bf665a39e10
Jobin <jobinthomas055@gmail.com>
2024-11-12 17:11:31 +0530

    second-commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,53 @@
 <script>
-    import banner from "$lib/assets/bg1.webp"
-    import tie from "$lib/assets/tie.webp"
+    import banner from "$lib/assets/bg1.webp";
+    import tie from "$lib/assets/tie.webp";
+    import material from "$lib/assets/material.png";
+    import MaterialTwo from "$lib/assets/MaterialTwo.jpg";
 </script>
+
 <section>
     <div class="relative">
         <p class="bg-[#003f72] bg-opacity-50 rounded-xl text-white p-20 leading-normal absolute z-10 top-[0vh] left-[0vw] 2xl:top-[10vh] 2xl:left-[5vw] md:w-[90vw] xl:w-[60vw] text-5xl">" MSW scaffolding utilizes an outstanding access system that effectively overcomes various engineering challenges."</p>
-        <img src="{banner}" class="md:w-full w-auto h-[70vh]  md:h-auto opacity-80" alt="">
+        <img src="{banner}" class="md:w-full w-auto h-[70vh] md:h-auto opacity-80 object-cover" alt="Banner">
     </div>
 </section>
 
-<section class="flex justify-center py-8 ">
-    <section class="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-12 xl:w-[60vw] ">
-        <img src="{tie}" class="h-full" alt="">
-        <div class="text-xl space-y-4 ">
+<section class="flex justify-center py-8">
+    <section class="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 gap-12 xl:w-[60vw]">
+        <img src="{tie}" class="h-full w-full object-cover" alt="Tie">
+        <div class="text-xl space-y-4">
             <h1 class="text-[#003f72]">More About Company</h1>
             <p>At Mainee Steel Works Pvt Ltd. (MSW), we take immense pride in being your trusted rental scaffolding service provider based in India. Our commitment is to offer specialized scaffolding solutions designed to meet the diverse needs of clients across India.</p>
             <p>Our team, comprised of experienced engineering professionals, possesses an in-depth understanding of the construction industry. We prioritize your requirements, recognizing the pivotal elements on a job site. Above all, your safety holds the utmost importance to us. Scaffolding safety isn't merely a consideration; it serves as the cornerstone of everything we do.</p>
             <p>At MSW, our commitment to scaffolding safety is reflected in various ways. As an ISO certified company, we adhere rigorously to the highest safety and environmental standards. Our dedication surpasses mere compliance, as we strive to set new industry benchmarks.</p>
             <p>Every scaffolding solution we offer embodies our commitment to safety, quality, and excellence. We aim to provide you with peace of mind on every job site.</p>
             <div>
-                <button class="bg-[#003f72] text-white hover:bg-black p-2">make appointment</button>
+                <button class="bg-[#003f72] text-white hover:bg-black p-2">Make Appointment</button>
             </div>
         </div>
     </section>
+</section>
+
+<section class="h-[80vh] w-full relative">
+    <h1 class="absolute left-[50%] top-48 transform -translate-x-[50%] -translate-y-[50%] text-6xl font-serif text-[#003f72] text-center">
+        Journey Of Sustainable Growth
+    </h1>
+    <div class="h-[40vh] w-[86%] relative top-[35%] flex gap-20 justify-center">
+        <div class="h-96 w-[60%]  relative">
+            <img src={material} alt="Material Selection" class="h-full w-full object-cover absolute left-24" />
+            <h1 class="absolute bottom-0 left-24 w-full text-center text-white bg-black opacity-70 py-2 text-3xl font-semibold">Material Selection</h1>
+        </div>
+        <div class="h-96 w-[60%] relative ">
+            <img src={MaterialTwo} alt="Material Two" class="h-full w-full object-cover absolute left-32" />
+            <h1 class="absolute bottom-0 left-32 w-full text-center text-white bg-black opacity-70 py-2 text-3xl font-semibold">Material Selection</h1>
+        </div>
+        <div class="h-96 w-[60%] relative">
+            <img src={MaterialTwo} alt="Material Two" class="h-full w-full object-cover absolute left-36" />
+            <h1 class="absolute bottom-0 left-36 w-full text-center text-white bg-black opacity-70 py-2 text-3xl font-semibold">Material Selection</h1>
+        </div>
+        <div class="h-96 w-[60%]  relative">
+            <img src={MaterialTwo} alt="Material Two" class="h-full w-full object-cover absolute left-40" />
+            <h1 class="absolute bottom-0 left-40 w-full text-center text-white bg-black opacity-70 py-2 text-3xl font-semibold">Material Selection</h1>
+        </div>
+    </div>
 </section>
